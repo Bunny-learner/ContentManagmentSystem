da58a06cbc92bfab2ea772154cc005c78183c096
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-03-11 21:50:31 +0530

    Delete backend directory

--- backend/models/weatherschema.js
+++
@@ -1,9 +0,0 @@
-import mongoose,{model, Schema} from 'mongoose'
-import jwt from 'jsonwebtoken'
-
-const weatherschema=new Schema({
-
-})
-
-export const weathermodel=new mongoose.model('weatherdata',weatherschema)
-
